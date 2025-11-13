18953b793c11a8c4cb6b16fd76f11663ebe3e761
Aavash Dulal <dulalaavas@gmail.com>
2025-11-13 14:41:34 +0545

    feat: Add user profile stats and image
    - Add a new Row to display user profile information including an avatar, posts, followers, and following counts.
    - Import a new image (`avshsq.jpg`) for the user avatar.
    - Style the profile image as a circle and arrange stats in Columns.
    - Update existing Row layouts with padding, alignment, and arrangement modifiers.

--- app/src/main/java/com/example/dsgavs/DashboardActivity.kt
+++ app/src/main/java/com/example/dsgavs/DashboardActivity.kt
@@ -5,18 +5,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dsgavs.ui.theme.DSGAVSTheme
 
 class DashboardActivity : ComponentActivity() {
@@ -37,7 +46,13 @@ fun ProfileBody(){
             modifier = Modifier
                 .padding(padding)
         ){
-            Row{
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
                 Icon(
                     painter = painterResource(
                             id = R.drawable.baseline_arrow_back_ios_new_24),
@@ -49,6 +64,36 @@ fun ProfileBody(){
                         id = R.drawable.baseline_more_horiz_24),
                     contentDescription = null
                 )
+            }
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround) {
+                Image(
+                    painter = painterResource(R.drawable.avshsq),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(90.dp)
+                        .clip(shape = CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("300")
+                    Text("Posts")
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("1.2M")
+                    Text("Followers")
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("9")
+                    Text("Followings")
+                }
             }
         }
     }
