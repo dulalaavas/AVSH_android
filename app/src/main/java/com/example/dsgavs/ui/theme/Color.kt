8affaea609d957c19e2524b0f9956293163ca285
Aavash Dulal <dulalaavas@gmail.com>
2025-11-20 00:54:06 +0545

    feat: Implement login screen UI
    - Create `LoginActivity` and set it as the launcher activity in `AndroidManifest.xml`.
    - Design the login screen layout using Jetpack Compose, including title, description, and social media login options (Facebook, Google).
    - Add email and password `OutlinedTextField`s with a password visibility toggle.
    - Include a "Log In" button and text for "Forget Password" and "Sign up".
    - Add new drawable resources for visibility icons (`baseline_visibility_24`, `baseline_visibility_off_24`), and social media logos (`facebook.png`, `google.png`).
    - Define new color values (Gray, Blue, White, Black) in `Color.kt`.
    - Add a new string resource for the `LoginActivity` title.

--- app/src/main/java/com/example/dsgavs/ui/theme/Color.kt
+++ app/src/main/java/com/example/dsgavs/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Gray = Color(0xFF6C6C6C)
+
+val Blue = Color(0xFF2D55DE)
+
+val White = Color(0xFFFFFFFF)
+
+val Black = Color(0xFF000000)
